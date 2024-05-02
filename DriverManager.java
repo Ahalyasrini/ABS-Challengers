f562c6badec34def6b36e6319ec6ca297ff4121d
buvana26 <167450942+buvana26@users.noreply.github.com>
2024-05-02 00:25:18 -0400

    Add files via upload

---
+++ DriverManager.java
@@ -0,0 +1,66 @@
+package dsalgo_webdriver_manager;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import dsalgoPOM.HomePage;
+import dsalgo_utilities.LoggerLoad;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverManager {
+	public static WebDriver driver;
+	public static Properties prop;
+	public DriverManager()
+	{	
+		try {
+		 prop = new Properties();
+		//FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//config//globaldata.properties");
+		FileInputStream fis = new FileInputStream("/Users/omkku/Desktop/Bhuvana_Selenium/Ds-Algo/"
+				+ "src/test/resources/config/globaldata.properties");			
+		prop.load(fis);			
+		} catch (IOException e) {
+			e.getMessage();
+		}
+	}
+	public static void initializeDriver() 
+	{			   	    	
+	    
+			if(driver==null)
+			{
+			if(prop.getProperty("browser").equalsIgnoreCase("chrome"))
+			{
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+			else if(prop.getProperty("browser").equalsIgnoreCase("firefox"))
+			{
+				driver = new FirefoxDriver();
+			}
+			else if(prop.getProperty("browser").equalsIgnoreCase("Edge"))
+			{
+				driver = new EdgeDriver();
+			}			
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get(prop.getProperty("Url"));
+			}
+		} 
+}
+	
+	/*public HomePage LaunchApplication() throws IOException
+	{
+		driver = initializeDriver();
+		HomePage homepage=new HomePage(driver);
+		homepage.goTo();
+		return homepage;
+	}
+*/
+	
